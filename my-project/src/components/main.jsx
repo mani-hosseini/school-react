0d2755355ee4c0f55a26d52a843eae7fc251143f
Mani hosseini <mani.hosseini.h8@gmail.com>
2025-02-12 02:32:53 +0330

    fix bug

--- my-project/src/components/main.jsx
+++ my-project/src/components/main.jsx
@@ -2,7 +2,7 @@ import {Baner} from "./baner/baner.jsx";
 
 export function Main() {
     return (
-        <main className={'lg:w-[80%] m-auto w-[95%]  mt-[72px]'}>
+        <main className={'lg:w-[80%] m-auto w-[95%] mt-[72px]'}>
             <Baner/>
         </main>
     );
