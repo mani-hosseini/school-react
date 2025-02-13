a25e889a65cbb921b6be0bbcd16218741f875678
Mani hosseini <mani.hosseini.h8@gmail.com>
2025-02-13 12:25:17 +0330

    byuld tittle

--- my-project/src/components/main.jsx
+++ my-project/src/components/main.jsx
@@ -1,9 +1,11 @@
 import {Baner} from "./baner/baner.jsx";
+import {Heading} from "./heading/heading.jsx";
 
 export function Main() {
     return (
         <main className={'lg:w-[80%] m-auto w-[95%] mt-[72px]'}>
             <Baner/>
+            <Heading/>
         </main>
     );
 
